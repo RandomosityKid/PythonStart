9fc912f96e3e9cf774ad97e555bbcc5d2dbb3deb
RandomosityKid <brant_dc@yahoo.com>
2023-03-22 18:21:36 -0700

    test: variables and input
    Unlike java, python is a dynamically typed language
    
    also tested inputs. looks like input is an immediate inquiry to the user

---
+++ basics/inputVariables.py
@@ -0,0 +1,9 @@
+#Variables
+name = input("What is your name: ")
+age = int(input("Enter age: "))
+numDouble = float(input("Enter a decimal number: "))
+
+#Print out values
+print("Name: ", name)
+print("Age: ", age)
+print("Decimal Number: ", numDouble)
